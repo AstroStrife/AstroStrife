443d22bb0191ec622874faf5deefbaf12a0082ca
leenawatH <98454141+leenawatH@users.noreply.github.com>
2024-05-20 23:38:35 +0700

    Delete Player directory

--- Player/AbilityShipData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New AbilityShipData", menuName = "Ability System/Ability Ship Data")]
-public class AbilityShipData : ScriptableObject
-{
-    public Ability ability1;
-    public Ability ability2;
-    public Ability ability3;
-}
