fea9ef0060fc06dc7910751311298fe35c617d16
leenawatH <98454141+leenawatH@users.noreply.github.com>
2024-05-21 00:22:54 +0700

    Add files via upload

---
+++ Player/Ability/AbilityShipData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New AbilityShipData", menuName = "Ability System/Ability Ship Data")]
+public class AbilityShipData : ScriptableObject
+{
+    public Ability ability1;
+    public Ability ability2;
+    public Ability ability3;
+}
